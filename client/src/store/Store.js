b0c0d091afd1318220f9fcb9eeeb298b100772c1
Frances Khem Andre Donaire <127834943+Donaire23@users.noreply.github.com>
2024-03-09 02:07:27 +0800

    Update Store.js

--- client/src/store/Store.js
+++ client/src/store/Store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import register from '../actions/reg';
+import user from '../actions/credentials'
 import login from '../actions/login';
+
 
 const store = configureStore({
     reducer: {
         Register: register,
-        Login: login
+        Login: login,
+        test: user
     }
 })
 
